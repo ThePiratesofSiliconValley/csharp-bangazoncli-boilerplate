2028506ee697b3bb439e0e83d3109d661d1ebb0d
Ben Harrington <harringtonben@icloud.com>
2018-03-05 21:11:59 -0600

    cleaned up sql data placement

---
+++ csharp-bangazoncli.app/DataAccess/AddProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_bangazoncli.app.DataAccess
+{
+    class AddProduct
+    {
+        readonly string _connectionString = ConfigurationManager.ConnectionStrings["BangazonCLI"].ConnectionString;
+        
+
+    }
+}
