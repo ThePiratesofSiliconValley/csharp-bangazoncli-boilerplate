812cec021a920113e3d4552c5912016de57a6f0e
David Dassau <todieahero@hotmail.com>
2018-03-05 20:06:30 -0600

    deleted old class, and created DataAccess folder and put CreateCustomerAccount class in it

--- csharp-bangazoncli.app/DataAccess/CreateCustomerAccount.cs
+++ csharp-bangazoncli.app/DataAccess/CreateCustomerAccount.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace csharp_bangazoncli.app
+namespace csharp_bangazoncli.app.DataAccess
 {
     class CreateCustomerAccount
     {
