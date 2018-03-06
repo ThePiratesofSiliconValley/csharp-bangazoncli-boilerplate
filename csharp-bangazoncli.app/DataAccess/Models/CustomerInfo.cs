b6f050ad2ff93585c08d970571582db00218a5e6
David Dassau <todieahero@hotmail.com>
2018-03-05 20:43:12 -0600

    added CustomerInfo.cs to Models

---
+++ csharp-bangazoncli.app/DataAccess/Models/CustomerInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace csharp_bangazoncli.app.DataAccess.Models
+{
+    class CustomerInfo
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Phone { get; set; }
+    }
+}
