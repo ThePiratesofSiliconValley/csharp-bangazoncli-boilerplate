fd56028b30256568a712d108737130ce62017786
David Dassau <todieahero@hotmail.com>
2018-03-08 21:38:34 -0600

    added _connectionString line and method to get the GetAllProducts List

---
+++ csharp-bangazoncli.app/DataAccess/RemoveProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using csharp_bangazoncli.app.DataAccess.Models;
+
+namespace csharp_bangazoncli.app.DataAccess
+{
+    class RemoveProduct
+    {
+        readonly string _connectionString = ConfigurationManager.ConnectionStrings["BangazonCLI"].ConnectionString;
+
+        public FuckinMethod()
+        {
+            var productQuery = new ProductQuery(); // Calling the ProductQuery class, to get the GetAllProducts List
+
+            var getAllProducts = productQuery.GetAllProducts(); // Assigns the GetAllProducts List to the getAllProducts variable
+
+
+        }
+    }
+}
