1d652623db510feeddcc3012f86ccec926ae6f79
David Dassau <todieahero@hotmail.com>
2018-03-12 17:47:23 -0500

    added CheckOrderLine method

--- csharp-bangazoncli.app/DataAccess/RemoveProduct.cs
+++ csharp-bangazoncli.app/DataAccess/RemoveProduct.cs
@@ -1,49 +1,54 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Data.SqlClient;
-//using System.Configuration;
-//using System.Data;
-//using csharp_bangazoncli.app.DataAccess.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using csharp_bangazoncli.app.DataAccess.Models;
 
-//namespace csharp_bangazoncli.app.DataAccess
-//{
-//    class RemoveProduct
-//    {
-//        readonly string _connectionString = ConfigurationManager.ConnectionStrings["BangazonCLI"].ConnectionString;
+namespace csharp_bangazoncli.app.DataAccess
+{
+    class RemoveProduct
+    {
+        readonly string _connectionString = ConfigurationManager.ConnectionStrings["BangazonCLI"].ConnectionString;
 
-//        public RemoveCustomerProduct(CustomerList customer)
-//        {
-//            var productQuery = new ProductQuery(); // Calling the ProductQuery class, to get the GetAllProducts List
+        public RemoveCustomerProduct(CustomerList customer)
+        {
+            var productQuery = new ProductQuery(); // Calling the ProductQuery class, to get the GetAllProducts List
 
-//            var getAllProducts = productQuery.GetCustomerProducts(customer.customerId); // Assigns the GetAllProducts List to the getAllProducts variable
+            var getAllProducts = productQuery.GetCustomerProducts(customer.CustomerId); // Assigns the GetAllProducts List to the getAllProducts variable
 
-//            var counter = 0;
-//            foreach (var product in getAllProducts)
-//            {
-//                counter++;
-//                Console.WriteLine($"{counter}. {product.ProductName}");
-//            }
+            var counter = 0;
+            foreach (var product in getAllProducts)
+            {
+                counter++;
+                Console.WriteLine($"{counter}. {product.ProductName}");
+            }
 
-//            var selectedProduct = int.Parse(Console.ReadLine());
+            var selectedProduct = int.Parse(Console.ReadLine());
 
-//            var productToDelete = getAllProducts[selectedProduct - 1];
+            var productToDelete = getAllProducts[selectedProduct - 1];
 
-//            var deleteProduct = DeleteProduct(productToDelete.ProductId);
-//        }
+            var deleteProduct = DeleteProduct(productToDelete.ProductId);
+        }
 
-//        public DeleteProduct(int productId)
-//        {
-//            using (var connection = new SqlConnection(_connectionString))
-//            {
-//                var cmd = connection.CreateCommand();
-//                cmd.CommandText = @"select ProductId from Products";
-//                connection.Open();
+        public CheckOrderLine(int productId)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = @"select count(*) from oderline
+                                    where ProductId = @productId";
+                connection.Open();
 
-//                var reader = cmd.ExecuteReader();
-//            }
-//        }
-//    }
-//}
+                var productIdParam = new SqlParameter("@productId", SqlDbType.Int);
+                productIdParam.Value = productId;
+                cmd.Parameters.Add(productIdParam);
+                
+
+            }
+        }
+    }
+}
