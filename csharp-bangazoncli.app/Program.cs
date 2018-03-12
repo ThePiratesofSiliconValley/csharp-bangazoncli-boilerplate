cf138326a6003f3fcee6c4ffdc2742abe4eefc11
David Dassau <todieahero@hotmail.com>
2018-03-12 18:24:18 -0500

    still working on program

--- csharp-bangazoncli.app/Program.cs
+++ csharp-bangazoncli.app/Program.cs
@@ -191,10 +191,12 @@ namespace csharp_bangazoncli.app
                     case 7:
                         Console.Clear();
                         //Remove customer product
+                        Console.WriteLine("Please choose product to delete:");
+
                         var removeCustomerProduct = new RemoveProduct();
                         removeCustomerProduct.RemoveCustomerProduct(customer);
 
-                        Console.WriteLine("Please choose product to delete:");
+                        
                         break;
                     case 8:
                         Console.Clear();
